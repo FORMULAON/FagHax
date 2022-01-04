1f74feab1b074cabf7629c9e4c1b10c7449a6049
KRYMZ0N <jackgrimes05@yahoo.com>
2022-01-03 20:33:50 -0500

    working on some possible crystal aura stuff.

--- src/main/java/org/crackedkittys/faghax/mod/pvp/CrystalAura.java
+++ src/main/java/org/crackedkittys/faghax/mod/pvp/CrystalAura.java
@@ -1,5 +1,8 @@
 package org.crackedkittys.faghax.mod.pvp;
 
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.crackedkittys.faghax.mod.Category;
 import org.crackedkittys.faghax.mod.Module;
 import org.lwjgl.glfw.GLFW;
@@ -8,5 +11,18 @@ public class CrystalAura extends Module {
     public CrystalAura() {
         super("Crystal Aura!", "Auto Destroys Crystals", Category.PVP, true);
         this.setKey(GLFW.GLFW_KEY_C);
+    }
+
+    public void onEnable() {
+        crystalPlace();
+
+    }
+
+    public void crystalPlace() {
+        UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            player.sendMessage(Text.of("Test"), false);
+
+            return ActionResult.PASS;
+        });
     }
 }
